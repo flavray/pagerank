993fbaeb5861ed0a480b9963b5cf432d5bd00963
Flavien Raynaud <raynaudfla@eisti.eu>
2014-10-05 12:20:44 +0000

    Start HTML parser

---
+++ src/Parser.java
@@ -0,0 +1,32 @@
+
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.util.ArrayList;
+
+/**
+ * Parser class is used to parse HTML pages and extract hyperlinks
+ */
+public class Parser {
+    private String mContent;
+    private ArrayList<String> mHyperlinks;
+
+    public Parser(String content) {
+        mContent = content;
+    }
+
+    public ArrayList<String> getHyperlinks() {
+        if (mHyperlinks == null) {
+            mHyperlinks = new ArrayList<String>();
+
+            Document doc = Jsoup.parse(mContent);
+
+            for (Element link : doc.select("a[href]"))
+                mHyperlinks.add(link.attr("href"));
+        }
+
+        return mHyperlinks;
+    }
+}
